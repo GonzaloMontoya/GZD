8a8c17529d68ab163ced91390c4729b4b5477197
go.montoya <go.montoya@duocuc.cl>
2023-06-30 17:46:41 -0400

    Tabla de gestión no funcional

--- GZDjango/GZDjango/settings.py
+++ GZDjango/GZDjango/settings.py
@@ -124,3 +124,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = "/"
--- GZDjango/GroundZero/views.py
+++ GZDjango/GroundZero/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import VistaObra, Artista
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 # Create your views here.
 
@@ -7,36 +9,34 @@ def base(request):
     return render(request, 'GroundZero/base.html')
 
 def index(request):
-    return render(request, 'index.html')
-
-
-# def artistas(request):
-#     return render(request, 'artistas.html')
+    return render(request, 'GroundZero/index.html')
 
 def baseproducto(request):
-    return render(request, 'baseproducto.html')
+    return render(request, 'GroundZero/baseproducto.html')
 
 def baseingreso(request):
-    return render(request, 'baseingreso.html')
+    return render(request, 'GroundZero/baseingreso.html')
 
-def login(request):
-    return render(request, 'login.html')
-
-def register(request):
-    return render(request, 'register.html')
-
-#--------------------------
-# def obras(request):   
-    # return render(request, 'obras.html')
+def gestArtista(request):
+    return render(request, 'GroundZero/gestion/gestArtista.html')
 
 
-
+#Datos de la base 
 def obras(request):
     obras = VistaObra.objects.all()
     context = {"obras":obras}
-    return render(request, 'obras.html', context)
+    return render(request, 'GroundZero/obras.html', context)
 
 def artistas(request):
     artistas  = Artista.objects.all()
     context = {"artistas":artistas}
-    return render(request, 'artistas.html', context)
+    return render(request, 'GroundZero/artistas.html', context)
+
+
+
+#Gestión ingreso
+def login(request):
+    return render(request, "GroundZero/login.html")
+
+def register(request):
+    return render(request, "GroundZero/register.html")
